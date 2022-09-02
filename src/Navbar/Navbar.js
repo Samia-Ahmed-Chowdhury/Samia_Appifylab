12cb407a21d4b8134b3395498762d02313b9b5e3
Samia-Ahmed-Chowdhury <samiachowdhury546@gmail.com>
2022-09-03 00:38:16 +0600

    c

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -29,6 +29,7 @@ function Navbar() {
                                     </button>
                                 </div>
                             </form>
+
                         </div>
                         <div className='widget '>
                             <ul className='d-flex flex-row'>
@@ -41,7 +42,7 @@ function Navbar() {
                                 </li>
 
                                 <li>
-                                    <NavLink className="nav-link "  to="/">
+                                    <NavLink className="nav-link " aria-current="page" to="/">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
                                         </svg>
@@ -49,7 +50,7 @@ function Navbar() {
                                 </li>
 
                                 <li>
-                                    <NavLink className="nav-link "  to="/">
+                                    <NavLink className="nav-link " aria-current="page" to="/">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
                                         </svg>
@@ -57,19 +58,19 @@ function Navbar() {
                                 </li>
 
                                 <li>
-                                    <NavLink className="nav-link "  to="/">
+                                    <NavLink className="nav-link " aria-current="page" to="/">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 01-2.555-.337A5.972 5.972 0 015.41 20.97a5.969 5.969 0 01-.474-.065 4.48 4.48 0 00.978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25z" />
                                         </svg>
                                     </NavLink>
                                 </li>
                                 <li className='search_li'>
-                                    <svg  className="search_icon w-6 h-6" xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" >
+                                    <svg className="search_icon w-6 h-6" xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" >
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                                     </svg>
                                 </li>
                                 <li className='user'>
-                                    <NavLink className="nav-link "  to="/">
+                                    <NavLink className="nav-link " aria-current="page" to="/">
                                         <AccountCircleIcon />
                                         <span>  Adnan Chowdhury <KeyboardArrowDownIcon /> </span>
                                     </NavLink>
@@ -79,7 +80,7 @@ function Navbar() {
                         </div>
 
 
-                        <button className="navbar-toggler" type="button" id="navbar-toggler">
+                        <button className="navbar-toggler" type="button" >
                             <label>
                                 <input className="toggle_btn" type="checkbox" id="toggle_btn" onClick={() => setShow(!show)} />
                                 <div className="toggle">
@@ -93,53 +94,124 @@ function Navbar() {
                     </div>
 
 
+                    {
+                        show ? 
+                        <div className="slide" id="slide">
+                            <ul className="navbar-nav  me-auto mb-lg-0 m-auto">
+
+                                <li className='account'>
+                                    <NavLink className="nav-link " aria-current="page" to="/">
+                                        <AccountCircleIcon className='account_svg' />
+                                        <span>  Adnan Chowdhury <KeyboardArrowDownIcon /> </span>
+
+                                    </NavLink>
+
+                                </li>
 
 
+                                <li>
+                                    <h2 className=''>Your Friends</h2>
+                                    <div class=" frnds_row container-fluid d-flex">
+                                        <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2  m-auto d-flex flex-column ">
+                                            <AccountCircleIcon />
+                                            <span>  Adnan Chowdhury </span>
+                                        </div>
+                                        <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2  m-auto d-flex flex-column ">
+                                            <AccountCircleIcon />
+                                            <span>  Adnan Chowdhury </span>
+                                        </div>
+                                        <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2  m-auto d-flex flex-column">
+                                            <AccountCircleIcon />
+                                            <span>  Adnan Chowdhury </span>
+                                        </div>
+                                        <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2  m-auto d-flex flex-column ">
+                                            <AccountCircleIcon />
+                                            <span>  Adnan Chowdhury </span>
+                                        </div>
+                                        <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2  m-auto d-flex flex-column">
+                                            <AccountCircleIcon />
+                                            <span>  Adnan Chowdhury </span>
+                                        </div>
+                                        <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2  m-auto d-flex flex-column">
+                                            <AccountCircleIcon />
+                                            <span>  Adnan Chowdhury </span>
+                                        </div>
+                                        <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2  m-auto d-flex flex-column">
+                                            <AccountCircleIcon />
+                                            <span>  Adnan Chowdhury </span>
+                                        </div>
+                                        <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2  m-auto d-flex flex-column ">
+                                            <AccountCircleIcon />
+                                            <span>  Adnan Chowdhury </span>
+                                        </div>
+                                    </div>
 
- { 
-            /* <!-- ---------mbl nav-------------- --> */
+                                </li>
 
-                show ? <div className="slide" id="slide">
-                    <ul className="navbar-nav  me-auto mb-lg-0 m-auto">
-                        <li className="nav-item">
-                            <NavLink className="nav-link active" aria-current="page" to="/Home">Home</NavLink>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <NavLink className="nav-link dropdown-toggle" to="/page" id="navbarDropdown" role="button"
-                                data-bs-toggle="dropdown" aria-expanded="false">
-                                Features
-                            </NavLink>
-                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><NavLink className="dropdown-item" to="/D_management">Driver management</NavLink></li>
-                                <li><NavLink className="dropdown-item" to="/page">Fleet management</NavLink></li>
-                                <li><NavLink className="dropdown-item" to="/page">Scheduling</NavLink></li>
-                                <li><NavLink className="dropdown-item" to="/page">Payroll</NavLink></li>
-                                <li><NavLink className="dropdown-item" to="/page">Complianxe</NavLink></li>
-                                <li><NavLink className="dropdown-item" to="/page">Driver app</NavLink></li>
+
+                                <li>
+                                    <div className="container">
+                                        <h2 className=''>Explore</h2>
+
+                                        <div className="col-12 d-flex justify-content-between my-5">
+                                            <div className="">
+                                                <div className='explore_card card'>
+                                                    <img src='../images/saved.svg' alt="img" className='img-fluid' />
+                                                    <h5>Saved </h5>
+                                                </div>
+                                            </div>
+                                            <div className="">
+                                                <div className='explore_card card'>
+                                                    <img src='../images/ppl.svg' alt="img" className='img-fluid' />
+                                                    <h5>People</h5>
+                                                </div>
+                                            </div>
+                                            <div className="">
+                                                <div className='explore_card card'>
+                                                    <img src='../images/grps.svg' alt="img" className='img-fluid' />
+                                                    <h5>Groups</h5>
+                                                </div>
+                                            </div>
+                                            <div className="">
+                                                <div className='explore_card card'>
+                                                    <img src='../images/pages.svg' alt="img" className='img-fluid' />
+                                                    <h5>Pages</h5>
+                                                </div>
+                                            </div>
+                                            <div className="">
+                                                <div className='explore_card card'>
+                                                    <img src='../images/events.svg' alt="img" className='img-fluid' />
+                                                    <h5>Events</h5>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </li>
+                                <li>
+                                    <div className="container">
+                                        <div class="row">
+                                            <div class=" col-8">
+                                                <div class="row">
+                                                    <div class=" col-5  ">
+                                                        <img src='../images/app-store.svg' alt="img" className='img-fluid' />
+                                                    </div>
+                                                    <div class=" col-5  my-auto ">
+                                                        <img src='../images/play_store.svg' alt="img" className='img-fluid' />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+
                             </ul>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/page">Our story</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/page">Pricing</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/page">Testimonials</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/page">Contact Us</NavLink>
-                        </li>
-                        <div className="btn">
-                            <NavLink className="nav-link" to="/page">
-                                <button type="btn" className="nav_btn">Book a demo</button>
-                            </NavLink>
                         </div>
+                            : null
+                    }
 
-                    </ul>
-                </div>
-                 : null
-            }
+
+
 
 
 
